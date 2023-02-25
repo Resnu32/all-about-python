0095bfc473f021721cf8b29715ae7445cd593ee2
Resnu32 <118877654+Resnu32@users.noreply.github.com>
2023-02-26 02:23:55 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+                #MATERI PRINT
+#nama = 'resnu' #tipe# data string
+#usia = 16 #tipe int
+#Tinggi_badan = 180.8 #Tipe float
+#punya_pacar = True
+#punya_pacar = False
+
+#print(punya_pacar)
+#print('hellooo my name is ' + nama +  'usia saya adalah ' + str(usia) + 'tinggi badan sayaa ' + str(Tinggi_badan))
+#print("hellooo usia saya: " +  str(usia))
+#print("Tinggi badan saya : " + str(Tinggi_badan))
+#print('ganteng')
+
+                #INPUT DATA
+#nama  = (input('whats your name?'))
+#print(nama)
+
+                #  ALJABAR
+#a = input('masukan angka pertama ') #output 4
+#b = input('masukan angka kedua ') #output 5
+#c = int(a) + int(b)#----->> int 4+5
+
+#+-*/
+
+#print('hasil ' + str(c))
+
+            #TUGAS
+#saldo_awal = input('berapa saldo awalmu: Rp')
+#deposit = input('berapa mau depositmu: Rp')
+#hasil = int(saldo_awal) + int(deposit)
+
+#print('jadi saldo akhirmu: Rp' + str(hasil))
+
+            #STRING
+#nama_saya = 'RESNU ADITYAAAAAAAAa'
+#print(nama_saya.find('A'))#mencari jumlah huruf tertentu
+#print(len(nama_saya))#mencari jumlah huruf
+#print('R' in nama_saya)#mencari kebenaran huruf
+#print(nama_saya.upper())#mengubah semua huruf ke besar semua
+#print(nama_saya.capitalize())#mengubah hanya hruf awal yang kapital
+        #PERKONDISIAN
+#usia = 2
+        #KETERANGAN
+#== sama dengan
+# > lebih dari
+# < kurang dari
+# != tidak sama dengan
+# >= lebih dari sama dengan
+# <= kurang dari sama dengan
+
+#if usia == 20:#dibaca ===>jika usia sama dengan 20 maka
+ #   print('yoman gw mudaaa')
+#else:#dibaca --->jika usia bukan 20(kecuali)
+ #   print('ga cocok')
+
+#if usia >= 5 and usia <= 10:
+ #   print('hallo kids')
+#elif usia > 10 and usia <=20:
+ #   print('hallo bang')
+#else: #pengecualian
+#    print('ga cocok')
+
+        #LOOPING
+#Array adalah kumpulan nilai nilai dari gabungan variabel
+#pacar_saya = ['A', 'B', 'C']
+
+#for pacar in pacar_saya:#untuk memecah
+ #   print(pacar)
+awal = 1
+
+while awal <= 10:
+    print(awal)
+    awal = awal + 1
